d3b07076bcf7c841bb9c8337d3acb6df55626bca
Sweash Shrestha <89719384+yonkou0@users.noreply.github.com>
2024-02-05 09:08:25 +0545

    Cleanfile Updated
    run this file as administrator (Right click > Administrator).

---
+++ clean.bat
@@ -0,0 +1,22 @@
+
+@echo off
+echo Cleaning up temporary files...
+
+:: Clear the system temp folder
+echo Cleaning system %temp% folder...
+del /q /f /s "%temp%\*"
+for /d %%p in ("%temp%\*.*") do rmdir "%%p" /s /q
+
+:: Clear the Windows temp folder
+echo Cleaning Windows temp folder...
+del /q /f /s "%windir%\Temp\*"
+for /d %%p in ("%windir%\Temp\*.*") do rmdir "%%p" /s /q
+
+:: Clear the Prefetch folder
+echo Cleaning Prefetch folder...
+del /q /f /s "%windir%\Prefetch\*"
+
+::CODE CREATED BY Matthew Ahmed
+
+echo Cleanup complete.
+pause
